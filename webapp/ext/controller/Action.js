309dd92378d6f76d9f5be34094c1556e1652eaf3
BTavkhelidze <beka.tavkhelidze@leverx.com>
2026-01-03 15:35:33 +0400

    Add custom UI fragments and enhance annotations

--- webapp/ext/controller/Action.js
+++ webapp/ext/controller/Action.js
@@ -8,8 +8,21 @@ sap.ui.define(['sap/m/MessageToast'], function (MessageToast) {
      * @param oContext the context of the page on which the event was fired. `undefined` for list report page.
      * @param aSelectedContexts the selected contexts of the table rows.
      */
-    callFunction: function (oContext, aSelectedContexts) {
-      MessageToast.show('Custom handler invoked.');
+
+    OnCallAction: function () {
+      const oModel = this.getModel();
+
+      const oAction = oModel.bindContext('/mutate(...)');
+
+      oAction.setParameter('param', 'param');
+      oAction
+        .execute()
+        .then(function () {
+          sap.m.MessageToast.show('Action executed successfully!');
+        })
+        .catch(function (oError) {
+          sap.m.MessageBox.error(oError.message);
+        });
     },
   };
 });
